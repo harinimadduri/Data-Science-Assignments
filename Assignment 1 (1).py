7b41fb41138aeca0899c17e25a40d655a9c114ec
harinimadduri <56841866+harinimadduri@users.noreply.github.com>
2020-11-08 19:59:21 +0530

    Add files via upload

---
+++ Assignment 1 (1).py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Question 1
+B.RAINBOW
+
+
+# In[2]:
+
+
+#Question 2
+print("LETS UPGRADE")
+
+
+# In[6]:
+
+
+#Question 3
+sellingprice=int(input())
+costprice=int(input())
+if(sellingprice>costprice):
+    Profit=sellingprice-costprice
+    print("Profit")
+elif(costprice>sellingprice):
+    Loss=costprice-sellingprice
+    print("Loss")
+else:
+    print("Neither")
+
+
+# In[7]:
+
+
+#Question 4
+Euros=int(input())
+Rupees=87.85*Euros
+print(Rupees)
+
+
+# In[ ]:
+
+
+
+
